06c98ae1608b48d7d1ce629377161a2265d1a3ee
karan250605 <kirubakaranj250605@gmail.com>
2024-11-15 16:01:17 +0530

    Add files via upload

---
+++ MCtest.java
@@ -0,0 +1,54 @@
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+public class MCTest extends Application{
+    @Override
+    public void start(Stage primaryStage)
+    {
+primaryStage.setTitle("Test Question !");
+        Label labelfirst=new Label("what is 10 +20?");
+        Label labelresponse=new Label();
+    Button button=new Button("submit");
+        RadioButton radio1,radio2,radio3,radio4;
+        radio1=new RadioButton("10");
+        radio2=new RadioButton("20");
+        radio3=new RadioButton("30");
+        radio4=new RadioButton("40");
+        ToggleGroup question1=new ToggleGroup();
+        radio1.setToggleGroup(question1);
+        radio2.setToggleGroup(question1);
+        radio3.setToggleGroup(question1);
+        radio4.setToggleGroup(question1);
+        button.setDisable(true);
+        radio1.setOnAction(e->button.setDisable(false));
+        radio2.setOnAction(e->button.setDisable(false));
+        radio3.setOnAction(e->button.setDisable(false));
+        radio4.setOnAction(e->button.setDisable(false));
+        button.setOnAction(e->
+        {
+            if(radio3.isSelected()){
+                labelresponse.setText("Correct answer");
+                button.setDisable(true);
+            }
+            else{
+                labelresponse.setText("wrong answer");
+                button.setDisable(true);
+            }
+        });
+        VBox layout=new VBox(5);
+        layout.getChildren().addAll(labelfirst, radio1, radio2, radio3, radio4,button,labelresponse);
+    Scene scene1=new Scene(layout,400,250);
+        primaryStage.setScene(scene1);
+primaryStage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+}
